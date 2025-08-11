e191f7554b4ae3cc9653a0042b7d301758fb41e0
Pedro Henrique <65197732+Nickdyegars@users.noreply.github.com>
2025-08-10 21:51:20 -0300

    Configuração do .env
    Configurado as variáveis de ambiente
    Configuração do readme.MD

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -1,13 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from 'firebase/database';
+import { APIKEY, AUTHDOMAIN, PROJECTID, STORAGEBUCKET, MESSAGINGSENDERID, APPID } from '@env';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCs6MU0calAKkVTl2dKr0d6vULr5bmsj0k",
-  authDomain: "eduflex-df1a5.firebaseapp.com",
-  projectId: "eduflex-df1a5",
-  storageBucket: "eduflex-df1a5.firebasestorage.app",
-  messagingSenderId: "918722155170",
-  appId: "1:918722155170:web:424a741101a288256b7dfb"
+  apiKey: APIKEY,
+  authDomain: AUTHDOMAIN,
+  projectId: PROJECTID,
+  storageBucket: STORAGEBUCKET,
+  messagingSenderId: MESSAGINGSENDERID,
+  appId: APPID
 };
 
 // Initialize Firebase
